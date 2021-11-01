efac0005fde39580b6177c2a0ee6a55e1096d756
Abhinav Shukla <abhinavshukla@Abhinavs-Air.lan>
2021-11-01 19:11:21 -0400

    Services

---
+++ server/models/deathsSorted.js
@@ -0,0 +1,6 @@
+module.exports = class deathsSorted {
+    constructor(country, deathstotalDeathsleast) {
+        this.country = country
+        this.deathstotalDeathsleast = deathstotalDeathsleast
+    }
+}
